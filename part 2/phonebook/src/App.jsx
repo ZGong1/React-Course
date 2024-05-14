8cd60d71660d8a8c9f8b7c2ab4c7e0006afa6e74
Eric Peterson <ericpeterson@tamu.edu>
2024-05-14 12:34:32 -0500

    2.6

---
+++ part 2/phonebook/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = e => {
+    e.preventDefault()
+    var newObj = { name: newName }
+    setPersons(persons.concat(newObj))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(item => <p key={item.name}>{item.name}</p>)}
+    </div>
+  )
+}
+
+export default App
